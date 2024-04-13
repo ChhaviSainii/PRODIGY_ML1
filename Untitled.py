a657194117d5f6986f023b19a0fe222fdb354130
ChhaviSainii <166942569+ChhaviSainii@users.noreply.github.com>
2024-04-13 22:36:42 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+HouseDF = pd.read_csv('train.csv')
+
+
+# In[3]:
+
+
+HouseDF.head()
+
+
+# In[4]:
+
+
+HouseDF.info()
+
+
+# In[5]:
+
+
+HouseDF.columns
+
+
+# In[ ]:
+
+
+sns.pairplot(HouseDF)
+
+
+# In[ ]:
+
+
+sns.distplot(HouseDF['Price'])
+
+
+# In[ ]:
+
+
+numeric_columns = HouseDF.select_dtypes(include=['number'])
+sns.heatmap(numeric_columns.corr(), annot=True)
+
+
+# In[ ]:
+
+
+X = HouseDF[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+               'Avg. Area Number of Bedrooms', 'Area Population']]
+
+y = HouseDF['Price']
+
+
+# In[ ]:
+
+
+from sklearn.model_selection import train_test_split
+
+
+# In[ ]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+
+
+# In[ ]:
+
+
+from sklearn.linear_model import LinearRegression
+
+
+# In[ ]:
+
+
+lm = LinearRegression()
+
+
+# In[ ]:
+
+
+lm.fit(X_train,y_train)
+
+
+# In[ ]:
+
+
+print(lm.intercept_)
+
+
+# In[ ]:
+
+
+coeff_df = pd.DataFrame(lm.coef_,X.columns,columns=['Coefficient'])
+coeff_df
+
+
+# In[ ]:
+
+
+predictions = lm.predict(X_test)
+
+
+# In[ ]:
+
+
+plt.scatter(y_test,predictions)
+
+
+# In[ ]:
+
+
+sns.distplot((y_test-predictions),bins=50);
+
+
+# In[ ]:
+
+
+from sklearn import metrics
+
+
+# In[ ]:
+
+
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
+
+# In[ ]:
+
+
+results_df = pd.DataFrame({'Actual Prices': y_test, 'Predicted Prices': predictions})
+results_df.to_csv('predictions.csv', index=False)
+
+
+# In[ ]:
+
+
+
+
